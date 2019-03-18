a1f17d12dfab13ebf2dba3fa9ec6fb12ac98ecd7
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-03-18 13:29:08 +0300

    update sorting function

--- src/component.js
+++ src/component.js
@@ -15,14 +15,7 @@ class Component {
     const coords = [
       this._getRandomNum(this._about.length),
       this._getRandomNum(this._about.length)
-    ].sort((a, b) => {
-      if (a < b) {
-        return -1;
-      } else if (a > b) {
-        return 1;
-      }
-      return 0;
-    });
+    ].sort((a, b) => a - b);
     return descr.slice(...coords).join(`. `);
   }
   get template() {
