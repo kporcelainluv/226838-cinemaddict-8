a449c3deb5e739fcc991401a5f1afa93ae86eb0d
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-03-23 23:42:49 +0300

    Add FILTER TYPE enum

--- src/filtersMock.js
+++ src/filtersMock.js
@@ -1,8 +1,16 @@
+const FILTER_TYPES = {
+  all: "All Movies",
+  watchlist: "Watchlist",
+  history: "History",
+  favorites: "Favorites",
+  stats: "Stats"
+};
+
 const filtersData = [
-  { name: "All movies" },
-  { name: "Watchlist", amount: 1 },
-  { name: "History", amount: 1 },
-  { name: "Favorites", amount: 1 },
-  { name: `Stats` }
+  { name: "All movies", type: FILTER_TYPES.all },
+  { name: "Watchlist", amount: 1, type: FILTER_TYPES.watchlist },
+  { name: "History", amount: 1, type: FILTER_TYPES.history },
+  { name: "Favorites", amount: 1, type: FILTER_TYPES.favorites },
+  { name: `Stats`, type: FILTER_TYPES.stats }
 ];
 export { filtersData };
