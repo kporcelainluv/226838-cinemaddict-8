fad97fb669d82a6fd24094f259e4723a38adfe9f
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-03-24 12:32:40 +0300

    add ids

--- src/filtersMock.js
+++ src/filtersMock.js
@@ -7,13 +7,24 @@ export const FILTER_TYPES = {
 };
 
 const filtersData = [
-  { name: "All movies", type: FILTER_TYPES.all },
-  { name: "Watchlist", amount: 1, type: FILTER_TYPES.watchlist },
-  { name: "History", amount: 1, type: FILTER_TYPES.history },
-  { name: "Favorites", amount: 1, type: FILTER_TYPES.favorites },
+  { name: "All movies", type: FILTER_TYPES.all, id: "all" },
+  {
+    name: "Watchlist",
+    amount: 1,
+    type: FILTER_TYPES.watchlist,
+    id: "watchlist"
+  },
+  { name: "History", amount: 1, type: FILTER_TYPES.history, id: "history" },
+  {
+    name: "Favorites",
+    amount: 1,
+    type: FILTER_TYPES.favorites,
+    id: "favorites"
+  },
   {
     name: `Stats`,
-    type: FILTER_TYPES.stats
+    type: FILTER_TYPES.stats,
+    id: "stats"
   }
 ];
 export { filtersData };
