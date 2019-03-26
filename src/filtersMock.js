4db14403c1cdfa0a6d05817d8acadac4c1c4695e
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-03-26 13:54:44 +0300

    update amount of films on the start

--- src/filtersMock.js
+++ src/filtersMock.js
@@ -8,9 +8,9 @@ export const FILTER_TYPES = {
 
 const filtersData = [
   { name: "All movies", type: FILTER_TYPES.all },
-  { name: "Watchlist", amount: 1, type: FILTER_TYPES.watchlist },
-  { name: "History", amount: 1, type: FILTER_TYPES.history },
-  { name: "Favorites", amount: 1, type: FILTER_TYPES.favorites },
+  { name: "Watchlist", amount: 0, type: FILTER_TYPES.watchlist },
+  { name: "History", amount: 0, type: FILTER_TYPES.history },
+  { name: "Favorites", amount: 0, type: FILTER_TYPES.favorites },
   {
     name: `Stats`,
     type: FILTER_TYPES.stats
