62a1df64e80e963e292007a6313533857a1322ac
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-03-26 13:53:19 +0300

    add amount of films to filter

--- src/filter.js
+++ src/filter.js
@@ -4,11 +4,15 @@ import { createChart } from "./formStats.js";
 // import { FILTER_TYPE }
 
 class Filter extends Component {
-  constructor(data) {
+  constructor(data, watchedFilms, favoriteFilms, watchlistFilms) {
     super();
     this._name = data.name;
-    this._amount = data.amount || null;
+    this._amountPresense = data.hasOwnProperty("amount");
+    this._amount = data.amount;
     this._type = data.type;
+    this._watchedFilms = watchedFilms;
+    this._favoriteFilms = favoriteFilms;
+    this._watchlistFilms = watchlistFilms;
     this._filtersContainer = document.querySelector(".main-navigation");
 
     this._onFilter = this._onFilter.bind(this);
@@ -31,10 +35,21 @@ class Filter extends Component {
     if (this._name === `Stats`) {
       filter.className += ` main-navigation__item--additional`;
     }
-    if (this._amount) {
+    if (this._amountPresense) {
       const numberOfFilms = document.createElement("span");
       numberOfFilms.className = "main-navigation__item-count";
-      numberOfFilms.textContent = `${this._amount}`;
+
+      switch (this._type) {
+        case "Watchlist":
+          numberOfFilms.textContent = `${this._watchlistFilms}`;
+          break;
+        case "History":
+          numberOfFilms.textContent = `${this._watchedFilms}`;
+          break;
+        case "Favorite":
+          numberOfFilms.textContent = `${this._favoriteFilms}`;
+          break;
+      }
       filter.appendChild(numberOfFilms);
     }
     return filter;
