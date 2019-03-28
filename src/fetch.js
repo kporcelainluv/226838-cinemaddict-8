b277f93e9e7747f9ddb48f84487c021eed9d1b17
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-03-28 17:23:40 +0300

    add sending data to server, add f to convert data to server data

--- src/fetch.js
+++ src/fetch.js
@@ -37,6 +37,37 @@ const tranform = films => {
   return arr;
 };
 
+const toRaw = data => {
+  let film = {};
+  film.id = film.id;
+  film.film_info.description = data.descriptionText;
+  film.film_info.age_rating = data.age_rating;
+  film.film_info.total_rating = data.totalRating;
+  film.film_info.actors = data.actors;
+  film.film_info.title = data.name;
+  film.film_info.director = data.director;
+  film.film_info.alternative_title = data.alternativeName;
+  film.film_info.director = data.director;
+  film.film_info.genre = data.genre;
+  film.film_info.poster = data.poster;
+  film.film_info.release.date = data.releaseDate;
+  film.film_info.release.release_country = data.country;
+
+  film.film_info.runtimedata.duration = data.duration;
+  film.film_info.writers = data.writers || [];
+  film.comments = data.comments;
+  film.comments.date = data.comments.data;
+  film.comments.comment = data.comments.text;
+  film.comments.author = data.comments.author;
+  film.comments.emotion = data.comments.emotion;
+
+  film.user_details.already_watched = data.watched;
+  film.user_details.watchlist = data.watchlist;
+  film.user_details.favorite = data.favorite;
+  film.user_details.personal_rating = data.personalRating;
+  film.comments.length = data.amountOfComments;
+  return film;
+};
 const getData = (url, method, body) => {
   const headers = new Headers();
   headers.append("Authorization", "Basic yukz590ik29889a");
@@ -65,10 +96,17 @@ const getFilms = () => {
   });
 };
 
+const updateServerFilm = data => {
+  const url = `movies/${data.id}`;
+  const method = `PUT`;
+  const body = JSON.stringify(toRaw(data));
+  return getData(url, method, body).then(response => {
+    return response.json();
+  });
+};
+
 const addComment = ({ id, data }) => {};
 
 const addRating = ({ id, data }) => {};
 
-const deleteTask = ({ id }) => {};
-
-export { getFilms };
+export { getFilms, updateServerFilm };
