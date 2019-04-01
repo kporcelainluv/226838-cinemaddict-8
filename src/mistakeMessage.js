278439efa7096f02d7fae24ba1a736d8af9c369a
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-04-01 21:03:44 +0300

    remove unnecessary file

--- src/mistakeMessage.js
+++
@@ -1,7 +0,0 @@
-export const loadingMessage = () => {
-  const parent = document.querySelector(`.films-list__container`);
-  const template = document
-    .querySelector(`.loading-message`)
-    .msGetRegionContent.querySelector(`h1`);
-  parent.appendChild(template);
-};
